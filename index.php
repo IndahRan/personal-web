7f1615f24fa43d6e23f5dfd30e1e2798ea198f42
unknown <indahrani@student.telkomuniversity.ac.id>
2023-02-24 20:28:15 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Biodata Indah</title>
+    <link rel="stylesheet" type="text/css" href="
+    style.css">
+</head>
+<body>
+<!-- bagian atas (fitur) -->
+<div class="menu">
+    <div class="logo">
+        <h2>PERSONAL WEB</h2>
+    </div>
+
+    <div class="dasboard">
+        <ul>
+            <li><a href="">Profile</a></li>
+            <li><a href="">Education</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+</div>
+<!-- isi utama -->
+<div class="homepage">
+    <img src="gambar/ini.jpeg">
+    <h1>Hallo Everyone, Welcome To My Personal web</h1>
+    <h3><span></span></h3>
+    
+    
+    <a href=""><button>See More About Me</button></a>
+</div>
+
+<!-- Profile -->
+<div class="Profile">
+    <h1>Profile</h1>
+    <div class="about">
+        <p>My name is Indah Putri Maharani. I was born on Thursday, August 8, 2002 in Bandung. 
+        Now I am 20 years old. I am a student at Telkom University majoring in informatics.
+        The reason I chose the Informatics major is because it suits with my interest, which is to 
+        develop or create programs that can be useful for many people. I am very interested in AI and IoT. 
+        My hobbies are reading, cooking and enjoy being involved in social activities. In addition, I also like music and English. 
+        I won 1st place in the spelling bee category when I was in high school. I am someone who has
+        a high will and determination, is always interested in learning new things, likes to socialize, always 
+        prepares all activities with plans and targets and has a high interest in manufacturing and technology development.</p>
+
+    </div>
+</div>
+
+<!-- pendidikan -->
+<div class="list-pendidikan">
+    <h1>Education</h1>
+    <div class="sd">
+        <img src="gambar/sd1.png">
+            <p><h3>SDN Mekar Galih 2007 – 2010</h3></p>
+            <p>Jl. Sekemala No.73, Pasanggrahan, Kec. Ujung Berung</p>
+           
+            <p><h3>SDN Lengkong 2011 – 2013</h3></p>
+            <p>Jl. Raya Bojongsoang, Lengkong, Kec. Bojongsoang</p>   
+    </div>
+
+    <div class="smp">
+        <img src="gambar/smp.png">
+        <p><h3>SMP Telkom Bandung 2014 – 2016</h3></p>
+        <p>Jl. Radio Palasari Road, Sukapura, Kec. Dayeuhkolot</p>   
+    </div>
+
+    <div class="sma">
+        <img src="gambar/sma.png">
+        <p><h3>SMA Telkom Bandung 2017 – 2019</h3></p>
+        <p>Jl. Radio Palasari Road, Citeureup, Kec. Dayeuhkolot</p>   
+    </div>
+
+    <div class="kuliah">
+        <img src="gambar/kuliah.png">
+        <p><h3>Telkom University 2020 - Now</h3></p>
+        <p>Jl. Telekomunikasi. 1, Terusan Buahbatu - Bojongsoang, Sukapura, Kec. Dayeuhkolot</p>   
+    </div>
+
+</div>
+
+
+</body>
+</html>
