1cab75c1d77096d72396d20738e665ebedf10736
unknown <indahrani@student.telkomuniversity.ac.id>
2023-02-27 19:50:36 +0700

    second commit

--- index.php
+++ index.php
@@ -2,8 +2,10 @@
 <html>
 <head>
     <title>Biodata Indah</title>
-    <link rel="stylesheet" type="text/css" href="
-    style.css">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
+    <!-- Link Swiper's CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
+
 </head>
 <body>
 <!-- bagian atas (fitur) -->
@@ -32,19 +34,174 @@
 
 <!-- Profile -->
 <div class="Profile">
-    <h1>Profile</h1>
-    <div class="about">
-        <p>My name is Indah Putri Maharani. I was born on Thursday, August 8, 2002 in Bandung. 
-        Now I am 20 years old. I am a student at Telkom University majoring in informatics.
-        The reason I chose the Informatics major is because it suits with my interest, which is to 
-        develop or create programs that can be useful for many people. I am very interested in AI and IoT. 
-        My hobbies are reading, cooking and enjoy being involved in social activities. In addition, I also like music and English. 
-        I won 1st place in the spelling bee category when I was in high school. I am someone who has
-        a high will and determination, is always interested in learning new things, likes to socialize, always 
-        prepares all activities with plans and targets and has a high interest in manufacturing and technology development.</p>
-
-    </div>
+    <h1>Profile</h1>   
 </div>
+    <section>
+        <div class="swiper mySwiper container">
+            <div class="swiper-wrapper content">
+
+                <!-- slide 1 -->
+                <div class="swiper-slide card">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="gambar/a.jpeg" alt="">
+                        </div>
+
+                        <div class="media-icons">
+                            <i class="fa-solid fa-user-shakespeare"></i>
+                            <i class="fa-solid fa-language"></i>
+                            <i class="fa-solid fa-keyboard"></i>
+                            <i class="fa-brands fa-figma"></i>
+                        </div>
+
+                        <div class="nama">
+                            <span class="name">Indah Putri Maharani</span>
+                            <span class="sub">Bandung, 08 August 2002</span>
+                        </div>
+                    </div> 
+                </div>
+
+                <!-- slide 2 -->
+                <div class="swiper-slide card">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="gambar/telkom.jpg">
+                        </div>
+
+                        <div class="media-icons">
+                            <i class="fa-solid fa-person-dress"></i>
+                            <i class="fa-solid fa-building-columns"></i>
+                            <i class="fa-brands fa-gratipay"></i>
+                            <i class="fa-solid fa-people-group"></i>
+                            <i class="fa-solid fa-code"></i>
+                        </div>
+
+                        <div class="nama">
+                            <span class="name">A student at Telkom University</span>
+                            <span class="sub"><p>Majoring in Informatics. The reason I 
+                                chose the Informatics major is because it suits with my interest, 
+                                which is to develop or create programs that can be useful for many people.</p></span>
+                        </div>
+
+                        <div class="button">
+                        <button class="aboutme"><p><a href="https://telkomuniversity.ac.id/">See More</a></p>
+                            </button>
+                        </div>
+                    </div> 
+                </div>
+
+                <!-- slide 3 -->
+                <div class="swiper-slide card">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="gambar/like.jpg">
+                        </div>
+
+                        <div class="media-icons">
+                            <i class="fa-solid fa-person-dress"></i>
+                            <i class="fa-solid fa-building-columns"></i>
+                            <i class="fa-brands fa-gratipay"></i>
+                            <i class="fa-solid fa-people-group"></i>
+                            <i class="fa-solid fa-code"></i>
+                        </div>
+
+                        <div class="nama">
+                            <span class="name">Interested in AI, IoT, UI/UX</span>
+                            <span class="sub"><p>and many other things. In my free time, I like 
+                                to reading, cooking and enjoy being involved in social activities. 
+                                In addition, I also like music and study English. I won 1st place in the 
+                                spelling bee category when I was in high school. I also do swimming
+                                at the weekend so I can keep myself healthy.</p></span>
+                        </div>  
+                    </div> 
+                </div>
+
+                <!-- slide 4 -->
+                <div class="swiper-slide card">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="gambar/orga.jpg">
+                        </div>
+
+                        <div class="media-icons">
+                            <i class="fa-solid fa-person-dress"></i>
+                            <i class="fa-solid fa-building-columns"></i>
+                            <i class="fa-brands fa-gratipay"></i>
+                            <i class="fa-solid fa-people-group"></i>
+                            <i class="fa-solid fa-code"></i>
+                        </div>
+
+                        <div class="nama">
+                            <span class="name">Organization Experience</span>
+                            <span class="sub"><p>I am active in several organizations
+                                 and events on campus such as being a committee for 
+                                 welcoming new students and being a lab assistant for 
+                                 Islamic religion courses.  I always try to improve my skills
+                                and morals to be a better person. I do this by joining as a 
+                                volunteer so I can help each other people with love and dedication.</p></span>
+                        </div>       
+                    </div> 
+                </div>
+
+                <!-- slide 5 -->
+                <div class="swiper-slide card">
+                    <div class="card-content">
+                        <div class="image">
+                            <img src="gambar/apps.jpg">
+                        </div>
+
+                        <div class="media-icons">
+                            <i class="fa-solid fa-person-dress"></i>
+                            <i class="fa-solid fa-building-columns"></i>
+                            <i class="fa-brands fa-gratipay"></i>
+                            <i class="fa-solid fa-people-group"></i>
+                            <i class="fa-solid fa-code"></i>
+                        </div>
+
+                        <div class="nama">
+                            <span class="name">About Me</span>
+                            <span class="sub"><p>I am someone who has a high will and determination, 
+                                is always interested in learning new things, likes to socialize, always 
+                                prepares all activities with plans and targets and has a high interest in 
+                                manufacturing and technology development.</p></span>
+                        </div>
+
+                        <div class="button">
+                            <button class="aboutme"><p><a href="https://drive.google.com/file/d/1p8luqcv6WCgnfUz2AIcvC7cl_saZwVAy/view?usp=share_link
+                                ">See More</a></p>
+                            </button>
+                        </div>
+                    </div> 
+                </div>
+                
+
+            </div>
+        </div>
+        <div class="swiper-button-next"></div>
+        <div class="swiper-button-prev"></div>
+        <div class="swiper-pagination"></div>
+    </section>
+    <!-- Swiper JS -->
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+        var swiper = new Swiper(".mySwiper", {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            loop: true,
+            pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+            },
+            navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+            },
+        });
+    </script>
+
+
 
 <!-- pendidikan -->
 <div class="list-pendidikan">
@@ -77,6 +234,25 @@
     </div>
 
 </div>
+
+<!-- kontak -->
+<div class="kontak">
+    <h1>Contact</h1>
+    <div class="address">
+        <img src="gambar/alamat.png">
+        <p>Jl. Raya Bojongsoang No.120, Lengkong, Kec. Bojongsoang, Kabupaten Bandung, Jawa Barat 40287</p>
+    </div>
+
+    <div class="phone">
+        <img src="gambar/telepon.png">
+        <p>+62 8953 2538 9940</p>
+    </div>
+
+    <div class="email">
+        <img src="gambar/email.png">
+        <p>indahrani08@gmail.com</p>
+    </div>
+    
 
 
 </body>
